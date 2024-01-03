f3ff74fe39e7440ba313f6f7a25d6268b0daff85
John Romero <romerojohn1012@gmail.com>
2024-01-03 19:23:33 +0100

    added hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import '../styles/Hero.css'
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-name">
+        <h1>John Romero</h1>
+      </div>
+      <div className='hero-description'>
+        <h2>a full stack developer</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
