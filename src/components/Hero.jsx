189aaaac225a6a5c3275efecac204f1544723d4a
John Romero <romerojohn1012@gmail.com>
2024-01-18 16:29:54 +0100

    added lottiefile with scroll down icon animated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
 import '../styles/Hero.css'
+import Lottie from "react-lottie";
+import animationData from "../assets/lottie/scrolldown.json";
 
 function Hero() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <div className="hero-container">
       <div className="hero-name">
@@ -8,6 +19,13 @@ function Hero() {
       </div>
       <div className='hero-description'>
         <h2>a full stack developer</h2>
+      </div>
+      <div className='scroll-lottie'>
+        <Lottie
+          options={defaultOptions}
+          height={80}
+          width={80}
+        />
       </div>
     </div>
   )
