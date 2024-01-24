82b6081e3393f935da1beafd85fd3c868e931a0c
John Romero <romerojohn1012@gmail.com>
2024-01-25 00:50:17 +0100

    changing files

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -1,85 +1,39 @@
-// import '../styles/PageTransition.css';
-// import { useRef, useEffect } from "react";
-// import { Power4, gsap } from "gsap";
-
-// function PageTransition() {
-//   const trans = useRef(null);
-
-//   useEffect(() => {
-//     const tl = gsap.timeline();
-
-//     tl.fromTo(
-//       trans.current,
-//       {
-//         y: "100%",
-//       },
-//       {
-//         duration: 1,
-//         y: 0,
-//         ease: Power4.easeOut,
-//       }
-//     );
-
-//     tl.to(
-//       trans.current,
-//       {
-//         duration: 0.5,
-//         opacity: 0,
-//         ease: Power4.easeOut,
-//       },
-//     );
-
-//     tl.set(trans.current, { display: "none" });
-
-//     const observer = new IntersectionObserver(
-//       ([entry]) => {
-//         if (!entry.isIntersecting) {
-//           tl.set(trans.current, { display: "none" });
-//           observer.disconnect();
-//         }
-//       },
-//       { threshold: 0 }
-//     );
-
-//     observer.observe(trans.current);
-
-//     return () => {
-//       observer.disconnect();
-//     };
-//   }, []);
-
-//   return (
-//     <div>
-//       <div className='transition-effect' ref={trans}></div>
-//     </div>
-//   );
-// }
-
-// export default PageTransition;
-
 import { motion } from "framer-motion";
+import PropTypes from 'prop-types';
+import '../styles/PageTransition.css';
 
+function PageTransition({ OgComponent }) {
+  const handleExitComplete = () => {
+    console.log("Slide-out animation complete");
+  };
 
-function PageTransition(OgComponent) {
   return (
     <>
       <OgComponent/>
-      <motion.div
+      {/* <motion.div
+        key={"slide-in"}
         className="slide-in"
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 1 }}
-        transition={{ durantion: 1, ease: [0.22, 1, 0.36, 1] }}
-      />
+        initial={{ scaleY: 0, opacity: 0 }}
+        animate={{ scaleY: 1, opacity: 1}}
+        exit={{ scaleY: 0, opacity: 0 }}
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        onAnimationComplete={handleEntryComplete}
+      /> */}
       <motion.div
+        key={"slide-out"}
         className="slide-out"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ durantion: 1, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
+        onAnimationComplete={handleExitComplete}
       />
     </>
-  )
+  );
 }
+
+PageTransition.propTypes = {
+  OgComponent: PropTypes.func.isRequired,
+};
 
 export default PageTransition;
