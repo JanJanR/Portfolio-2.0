45c6ed8e64d6f60606af1d7582d788d8c3ea058e
John Romero <romerojohn1012@gmail.com>
2024-02-07 16:13:39 +0100

    starting the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import Navbar from "../components/Navbar"
+import '../styles/About.css'
 
 function About() {
   return (
     <>
       <Navbar />
-      <div>About</div>
+      <div className="about-container">
+        <div className="about-title">
+          <h1>About Me</h1>
+        </div>
+      </div>
     </>
   )
 }
