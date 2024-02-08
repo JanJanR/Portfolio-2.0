f47e6cf3a2068103fbc4bfb55576208870351f6f
John Romero <romerojohn1012@gmail.com>
2024-02-08 16:57:15 +0100

    changing about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,42 @@
 import Navbar from "../components/Navbar"
 import '../styles/About.css'
 
+import Typewriter from 'typewriter-effect';
+import Lottie from "react-lottie";
+import animationData from "../assets/lottie/scrolldown.json";
+
 function About() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <>
-      <Navbar />
-      <div className="about-container">
-        <div className="about-title">
-          <h1>Hello</h1>
-        </div>
+    <div className="about-container">
+      <div>
+        <Navbar />
       </div>
+        <div className="about-title">
+        <Typewriter
+          options={{
+            strings: ['Hi', 'Salut',' Ciao', 'Hola'],
+            autoStart: true,
+            loop: true,
+            delay: 200,
+          }}
+          />
+        </div>
+        <Lottie
+          options={defaultOptions}
+          height={80}
+          width={80}
+        />
+    </div>
     </>
   )
 }
