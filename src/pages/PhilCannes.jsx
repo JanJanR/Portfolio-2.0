a98e98f518343ca5dc393c6f8e503da05435b480
John Romero <romerojohn1012@gmail.com>
2024-03-05 14:22:20 +0100

    making the philcannes page

--- src/pages/PhilCannes.jsx
+++ src/pages/PhilCannes.jsx
@@ -1,10 +1,43 @@
+import "../styles/PhilCannes.css"
+
 import Navbar from "../components/Navbar";
+import PhilCannesHero from "../assets/philcannes/philcannes-bg-shadow.png";
 
 function PhilCannes() {
   return (
     <>
-      <Navbar/>
-      <div>PhilCannes</div>
+      <div className="philcannes-container">
+        <div className="philcannes-hero">
+          <img src={PhilCannesHero} alt="PhilCannesHero" className="philcannes-bg-shadow"/>
+          <Navbar/>
+          <div className="philcannes-hero-content">
+          <div></div>
+          <div></div>
+          <div className="philcannes-hero-right">
+            <h1>PhilCannes<br/>Detailing</h1>
+            <h3>
+              Collaboratively, we designed a user-friendly website for a boat detailing company,
+              simplifying the quote request process.
+              Users effortlessly input their boat details to receive instant,
+              personalized quotes, enhancing overall accessibility and efficiency in accessing premium detailing services.
+            </h3>
+            <div className="philcannes-description">
+              <h4>Date</h4>
+              <p>Dec 2022</p>
+            </div>
+            <div className="philcannes-description">
+              <h4>Role</h4>
+              <p>Designer, Dev</p>
+            </div>
+            <div className="philcannes-description">
+              <h4>Techs</h4>
+              <p>Figma, NextJS, Javascript</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div>PhilCannes</div>
+      </div>
     </>
   )
 }
