c2c06b1ed415171279b0f979ab461fc57d211e82
John Romero <romerojohn1012@gmail.com>
2024-03-06 16:51:36 +0100

    cartepub page

--- src/pages/CartePub.jsx
+++ src/pages/CartePub.jsx
@@ -1,10 +1,59 @@
+import "../styles/CartePub.css"
+
 import Navbar from "../components/Navbar";
 
+import Lottie from "react-lottie";
+import animationData from "../assets/lottie/scrolldown.json";
+
+import CartePubHero from "../assets/cartepub/cartepub-bg-shadow.png";
+
 function CartePub() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
     <>
-      <Navbar/>
-      <div>CartePub</div>
+      <div className="cartepub-container">
+        <div className="cartepub-hero">
+          <img src={CartePubHero} alt="cartepub-hero" className="cartepub-bg-shadow"/>
+          <Navbar/>
+          <div className="cartepub-hero-content">
+            <div></div>
+            <div></div>
+            <div className="cartepub-hero-right">
+              <h1>Carte Pub</h1>
+              <h3>
+                Presenting Carte Pub,
+                an innovative freelance project that reshapes advertising by prioritizing user interaction.
+                Through clickable features and personalized campaigns,
+                this app transforms passive ad viewing into an engaging experience for both advertisers and users.
+              </h3>
+              <div className="cartepub-description">
+                <h4>Date</h4>
+                <p>Mar 2024</p>
+              </div>
+              <div className="cartepub-description">
+                <h4>Role</h4>
+                <p>Designer, Dev</p>
+              </div>
+              <div className="cartepub-description">
+                <h4>Techs</h4>
+                <p>Figma, React Native, Javascript</p>
+              </div>
+            </div>
+          </div>
+          <Lottie
+            options={defaultOptions}
+            height={65}
+            width={65}
+          />
+        </div>
+      </div>
     </>
   )
 }
