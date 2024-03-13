83844b5ea57ebebb90a5fe1590c67c6dcd8b2e02
John Romero <romerojohn1012@gmail.com>
2024-03-13 12:18:54 +0100

    remove animation in the contact for the moment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,8 @@ import { IconBrandLinkedin } from '@tabler/icons-react';
 import emailjs from '@emailjs/browser';
 import { useRef } from "react";
 
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
 
 function Contact() {
 
@@ -32,109 +32,109 @@ function Contact() {
       });
   };
 
-  const tl = gsap.timeline();
-  const contactTitleRef = useRef(null);
-  const contactDescriptionRef = useRef(null);
-  const contactFormRef = useRef(null);
-  const contactGithubRef = useRef(null);
-  const contactMailRef = useRef(null);
-  const contactLinkedinlRef = useRef(null);
+  // const tl = gsap.timeline();
+  // const contactTitleRef = useRef(null);
+  // const contactDescriptionRef = useRef(null);
+  // const contactFormRef = useRef(null);
+  // const contactGithubRef = useRef(null);
+  // const contactMailRef = useRef(null);
+  // const contactLinkedinlRef = useRef(null);
 
-  useGSAP (() => {
-    tl.fromTo(contactTitleRef.current,{
-      x: -100,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      ease: "none"
-    })
+  // useGSAP (() => {
+  //   tl.fromTo(contactTitleRef.current,{
+  //     x: -100,
+  //     opacity: 0,
+  //   },
+  //   {
+  //     x: 0,
+  //     opacity: 1,
+  //     ease: "none"
+  //   })
 
-    tl.fromTo(contactDescriptionRef.current,{
-      x: -100,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      ease: "none"
-    })
+  //   tl.fromTo(contactDescriptionRef.current,{
+  //     x: -100,
+  //     opacity: 0,
+  //   },
+  //   {
+  //     x: 0,
+  //     opacity: 1,
+  //     ease: "none"
+  //   })
 
-    tl.fromTo(contactFormRef.current,{
-      opacity: 0
-    },
-    {
-      opacity: 1,
-      duration: 1,
-      ease: "none"
-    })
+  //   tl.fromTo(contactFormRef.current,{
+  //     opacity: 0
+  //   },
+  //   {
+  //     opacity: 1,
+  //     duration: 1,
+  //     ease: "none"
+  //   })
 
-    tl.fromTo(contactGithubRef.current,{
-      y: 10,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
+  //   tl.fromTo(contactGithubRef.current,{
+  //     y: 10,
+  //     opacity: 0,
+  //   },
+  //   {
+  //     y: 0,
+  //     opacity: 1,
 
-      ease: "none"
-    })
+  //     ease: "none"
+  //   })
 
-    tl.fromTo(contactMailRef.current,{
-      y: 10,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      ease: "none"
-    })
+  //   tl.fromTo(contactMailRef.current,{
+  //     y: 10,
+  //     opacity: 0,
+  //   },
+  //   {
+  //     y: 0,
+  //     opacity: 1,
+  //     ease: "none"
+  //   })
 
-    tl.fromTo(contactLinkedinlRef.current,{
-      y: 10,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      ease: "none"
-    })
-  })
+  //   tl.fromTo(contactLinkedinlRef.current,{
+  //     y: 10,
+  //     opacity: 0,
+  //   },
+  //   {
+  //     y: 0,
+  //     opacity: 1,
+  //     ease: "none"
+  //   })
+  // })
 
   return (
     <>
       <div className="contact-container">
-        <div className="contact-title" ref={contactTitleRef}>
+        <div className="contact-title">
           <h1>Contact</h1>
         </div>
         <div className="contact-content">
           <div className="left-contact">
-            <div className="contact-description" ref={contactDescriptionRef}>
+            <div className="contact-description">
               <h2>Whether you have a promising opportunity knocking at your door, or simply wish to extend a friendly greeting,
                   don&apos;t hesitate to get in touch with me; I&apos;m always open to engaging in conversations,exploring new possibilities,
                   and connecting with others in meaningful ways.
               </h2>
             </div>
             <div className="contact-social-media">
-              <div className="github" ref={contactGithubRef}>
+              <div className="github">
                 <a href="https://github.com/JanJanR" target="_blank" rel="noopener noreferrer">
                   <IconBrandGithub size={70} stroke={0.5} id="github"/>
                 </a>
               </div>
-              <div className="mail" ref={contactMailRef}>
+              <div className="mail">
                 <a href="mailto:romerojohn1012@gmail.com">
                   <IconMail size={70} stroke={0.5} id="mail"/>
                 </a>
               </div>
-              <div className="linkedin" ref={contactLinkedinlRef}>
+              <div className="linkedin">
                 <a href="https://www.linkedin.com/in/john-romero-272b22256/" target="_blank" rel="noopener noreferrer">
                   <IconBrandLinkedin size={70} stroke={0.5} id="linkedin"/>
                 </a>
               </div>
             </div>
           </div>
-          <div className="right-contact" ref={contactFormRef}>
+          <div className="right-contact">
             <form ref={form} onSubmit={sendEmail}>
               <h1>send me an email</h1>
               <label><h2>Name</h2></label>
