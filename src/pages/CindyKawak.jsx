ddfb59116f3047c8cd01aeb20372f5368211ef24
John Romero <romerojohn1012@gmail.com>
2024-03-19 13:32:25 +0100

    small changes

--- src/pages/CindyKawak.jsx
+++ src/pages/CindyKawak.jsx
@@ -1,10 +1,40 @@
 import Navbar from "../components/Navbar";
 import "../styles/cindykawak.css"
+import CindyKawakHero from "../assets/cindykawak/cindykawak-bg-shadow.png";
 
 function CindyKawak() {
   return (
     <>
-      <Navbar/>
+      <div className="cindykawak-container">
+        <div className="cindykawak-hero">
+          <img src={CindyKawakHero} alt="cindykawak-hero" className="cindykawak-bg-shadow" />
+          <Navbar/>
+          <div className="cindykawak-hero-content">
+            <div></div>
+            <div></div>
+            <div className="cindykawak-hero-right">
+              <h1>Cindy Kawak</h1>
+              <h3>
+                I worked with a restaurant consultant to create a sleek website featuring an exclusive e-book download.
+                The site offers valuable insights into restaurant management,
+                showcasing the potential of digital platforms to share expertise widely.
+              </h3>
+              <div className="cindykawak-description">
+                <h4>Date</h4>
+                <p>Dec 2023</p>
+              </div>
+              <div className="ciindykawak-description">
+                <h4>Role</h4>
+                <p>Designer, Dev</p>
+              </div>
+              <div className="ciindykawak-description">
+                <h4>Techs</h4>
+                <p>ReactJS, Figma, Javascript</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>CindyKawak</div>
     </>
   )
