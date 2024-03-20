b427fa1473169fdfe0eb11c7f587feab832db849
John Romero <romerojohn1012@gmail.com>
2024-03-20 16:10:03 +0100

    starting hero for airbroomnbroom

--- src/pages/AirbroomNBroom.jsx
+++ src/pages/AirbroomNBroom.jsx
@@ -1,10 +1,15 @@
+import "../styles/AirBroomNBroom.css"
+
 import Navbar from "../components/Navbar";
+
+import AirBroomNBroomHero from "../assets/airbroomnbroom/AirBroomNBroom-bg-shadow.png";
 
 function AirbroomNBroom() {
   return (
     <>
       <div className="airbroomnbroom-container">
         <div className="airbroomnbroom-hero">
+          <img src={AirBroomNBroomHero} alt="airbroomnbroomHero" />
           <Navbar/>
         </div>
         <div>AirbroomNBroom</div>
