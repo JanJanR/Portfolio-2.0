c2a14078efbabf1ba3b7a8896b94deaf0503a81c
John Romero <romerojohn1012@gmail.com>
2024-03-20 16:37:53 +0100

    finishing the page

--- src/pages/AirbroomNBroom.jsx
+++ src/pages/AirbroomNBroom.jsx
@@ -16,10 +16,27 @@ function AirbroomNBroom() {
             <div></div>
             <div className="airbroomnbroom-hero-right">
               <h1>AirBroomNBroom</h1>
+              <h3>
+                In our debut project, our team of four crafted a magical twist on Airbnb:
+                a platform offering broomstick rentals for travel.
+                Through collaborative effort and creativity,
+                we overcame challenges to deliver a unique and enchanting experience.
+              </h3>
+              <div className="airbroomnbroom-description">
+                <h4>Date</h4>
+                <p>Nov 2022</p>
+              </div>
+              <div className="airbroomnbroom-description">
+                <h4>Role</h4>
+                <p>Designer, Dev</p>
+              </div>
+              <div className="airbroomnbroom-description">
+                <h4>Techs</h4>
+                <p>Figma, Ruby on Rails, Javascript</p>
+              </div>
             </div>
           </div>
         </div>
-        <div>AirbroomNBroom</div>
       </div>
     </>
   )
