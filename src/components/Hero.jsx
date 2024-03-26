1e9d48e582efb709425ad322deec3fe55060d101
John Romero <romerojohn1012@gmail.com>
2024-03-26 14:40:25 +0100

    starting media query

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,6 @@
-import { useRef } from "react";
 import '../styles/Hero.css'
 import Lottie from "react-lottie";
 import animationData from "../assets/lottie/scrolldown.json";
-
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-
 
 function Hero() {
   const defaultOptions = {
@@ -17,64 +12,20 @@ function Hero() {
     }
   };
 
-  const tl = gsap.timeline();
-  const heroDescriptionRef = useRef(null);
-  const heroLottieRef = useRef(null);
-  const heroBoxNameRef = useRef(null);
-  const heroBoxLastNameRef = useRef(null);
-
-  useGSAP (() => {
-    tl.fromTo(heroBoxNameRef.current,{
-      x: 0,
-    },
-    {
-      x: '-150%',
-      ease: "none",
-      duration: 1
-    })
-
-    tl.fromTo(heroBoxLastNameRef.current,{
-      x: 0,
-    },
-    {
-      x: '-150%',
-      ease: "none",
-      duration: 1
-    })
-
-    tl.fromTo(heroDescriptionRef.current,{
-      opacity: 0
-    },
-    {
-      opacity: 1,
-      duration: 1
-    })
-
-    tl.fromTo(heroLottieRef.current,{
-      opacity: 0,
-    },
-    {
-      opacity: 1,
-      duration: 1.5
-    })
-  })
-
   return (
     <div className="hero-container">
       <div className="hero-name-container">
         <div className="hero-name">
-          {/* <div className="box-name" ref={heroBoxNameRef}></div> */}
           <h1>John</h1>
         </div>
         <div className="hero-lastname">
-          {/* <div className="box-lastname" ref={heroBoxLastNameRef}></div> */}
           <h1>Romero</h1>
         </div>
       </div>
-      <div className='hero-description' ref={heroDescriptionRef}>
+      <div className='hero-description'>
         <h2>a full stack developer</h2>
       </div>
-      <div className='scroll-lottie' ref={heroLottieRef}>
+      <div className='scroll-lottie'>
         <Lottie
           options={defaultOptions}
           height={80}
