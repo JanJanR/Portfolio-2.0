e2a0dfe77cdb3b7ba51bd9f36efe6b3c83e0f289
John Romero <romerojohn1012@gmail.com>
2024-03-26 16:19:13 +0100

    modal media query

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, useLocation} from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import Contact from "./pages/Contact";
 import AirbroomNBroom from "./pages/AirbroomNBroom";
 import CartePub from "./pages/CartePub";
 import CindyKawak from "./pages/CindyKawak";
@@ -15,7 +15,7 @@ const App = () => {
     <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      {/* <Route path="/contact" element={<Contact />} /> */}
       <Route path="/projects/AirbroomNBroom" element={<AirbroomNBroom />} />
       <Route path="/projects/CartePub" element={<CartePub />} />
       <Route path="/projects/CindyKawak" element={<CindyKawak />} />
